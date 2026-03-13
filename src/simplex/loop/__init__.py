f06006e7651b2435c011e9500d2c92a890a7cde3
Hazer <3169443421@qq.com>
2026-03-14 00:01:40 +0800

    2026/3/14-lcz

--- src/simplex/loop/__init__.py
+++ src/simplex/loop/__init__.py
@@ -1,11 +1,13 @@
 import simplex.loop.base
 
 from simplex.loop.base import AgentLoop, LoopAction, LoopStateEdit, LogExceptionHandler
+from simplex.loop.adapter import ParallelSampleAdapter
 
 
 __all__ = [
     "AgentLoop",
     "LoopAction",
     "LoopStateEdit",
-    "LogExceptionHandler"
+    "LogExceptionHandler",
+    "ParallelSampleAdapter"
 ]
